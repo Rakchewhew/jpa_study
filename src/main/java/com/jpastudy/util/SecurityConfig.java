17165198ad3ef8e1d080fcbc2900e4d745a4e9b6
kimkirak <kirak807807@gmail.com>
2024-03-08 19:06:29 +0900

    feat : security 기본 설정

--- src/main/java/com/jpastudy/util/SecurityConfig.java
+++ src/main/java/com/jpastudy/util/SecurityConfig.java
@@ -1,21 +1,44 @@
 package com.jpastudy.util;
 
+import jakarta.servlet.DispatcherType;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.CustomAutowireConfigurer;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.OrRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private static final RequestMatcher AUTH_WHITELIST = new OrRequestMatcher(
+            PathRequest.toStaticResources().atCommonLocations(),
+            new AntPathRequestMatcher("/")
+    );
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-        http.csrf(AbstractHttpConfigurer::disable);
-        http.authorizeHttpRequests(
-                authorize -> authorize.anyRequest().authenticated()
-        );
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((auth)
+                        -> auth
+                        .dispatcherTypeMatchers(DispatcherType.FORWARD)
+                        .authenticated().requestMatchers(AUTH_WHITELIST).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults())
+        ;
+
         return http.build();
     }
 }
