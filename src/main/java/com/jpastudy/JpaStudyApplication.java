44ce600c1785057e23e8939a3b8977edb243e814
kimkirak <kirak807807@gmail.com>
2024-03-08 19:03:37 +0900

    rename : JpaStudyApplication, JpaStudyApplicationTests

--- src/main/java/com/jpastudy/JpaStudyApplication.java
+++ src/main/java/com/jpastudy/JpaStudyApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JpastudyApplication {
+public class JpaStudyApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(JpastudyApplication.class, args);
+        SpringApplication.run(JpaStudyApplication.class, args);
     }
 
 }
