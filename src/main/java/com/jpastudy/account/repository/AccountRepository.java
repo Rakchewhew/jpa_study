7e79e9b64f3c831cc8c15948dc75ba7c4f1bb9dd
kimkirak <kirak807807@gmail.com>
2024-03-26 19:55:13 +0900

    fix : findAccountByLoginId 변경

--- src/main/java/com/jpastudy/account/repository/AccountRepository.java
+++ src/main/java/com/jpastudy/account/repository/AccountRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
-    Optional<Account> findAccountById(String loginId);
+    Optional<Account> findAccountByLoginId(String loginId);
 }
