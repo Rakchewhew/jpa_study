b687ab979281a357d3e444a9527ffb7554c7b6c4
kimkirak <kirak807807@gmail.com>
2024-03-26 19:20:56 +0900

    feat : AccountRepository 추가

---
+++ src/main/java/com/jpastudy/account/repository/AccountRepository.java
@@ -0,0 +1,10 @@
+package com.jpastudy.account.repository;
+
+import com.jpastudy.account.entity.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface AccountRepository extends JpaRepository<Account, Long> {
+    Optional<Account> findAccountById(String loginId);
+}
